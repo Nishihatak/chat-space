09d3613d22fc1aa255422651057ab58f41189362
Nishihatak <nishihata1451@gmail.com>
2019-10-21 21:49:58 +0900

    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : ""; //三項演算子を使ってmessage.imageにtrueならHTML要素、faiseなら空の値を代入。
+    image = (message.image.url) ? `<img class= "lower-message__image" src=${message.image.url} >` : ""; //三項演算子を使ってmessage.imageにtrueならHTML要素、faiseなら空の値を代入。
 
     var html = `<div class="message" data-message-id="${message.id}"> 
           <div class="upper-message">
@@ -8,7 +8,7 @@ $(function(){
               ${message.user_name}
             </div>
             <div class="upper-message__date">
-              ${message.date}
+              ${message.created_at }
             </div>
           </div>
           <div class="lower-meesage">
@@ -20,8 +20,7 @@ $(function(){
         </div>`
     return html;
   }
-
-  $('#new_message').on('submit', function(e){
+    $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -45,4 +44,30 @@ $(function(){
       alert("エラー")
     })
   })
-})
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';//追加するHTMLの入れ物を作る
+      messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
+        $('.messages').append(insertHTML);//メッセージを追加
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  }
+  setInterval(reloadMessages, 5000);
+});
